922edf0248467403cccce3ebea56f3fe92f5b615
Kristofer M White <me@kmwhite.net>
2013-10-22 11:56:22 -0500

    Adding base rails tree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Docket::Application.config.secret_key_base = '6ce5e487316e89bdcd1425ca1d671afb646f5294b3358c35e6d43fa27d06dd85fdbac244d936e981c1b33bccbf98314725ec9247f6960f2442eb3be5c9d76325'
