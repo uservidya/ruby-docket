1b7076f0964dbe2b58f2d1d95008346c53d757ce
Kristofer M White <me@kmwhite.net>
2013-10-31 13:17:37 -0500

    Adding base blueprints

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require File.expand_path(File.dirname(__FILE__) + '/blueprints')
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
