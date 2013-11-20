b896b9a08926edfa5ff12c74f58b74472fa49cb5
Jesse R. Adams <jesse@techno-geeks.org>
2013-11-20 15:43:20 -0800

    Adding basic capybara-webkit structure with sample test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require File.expand_path(File.dirname(__FILE__) + '/blueprints')
+require 'capybara/rails'
+
+Capybara.javascript_driver = :webkit
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
@@ -20,4 +23,8 @@ end
 
 class ActionController::TestCase
   include Devise::TestHelpers
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
 end
